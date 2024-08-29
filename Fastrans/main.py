ecf63f4ba46bf3239a0ed904af9344bb9767e337
Yaoqx <1064245198@qq.com>
2024-08-29 13:22:03 +0800

    使用 pyqt-fluent-widgets UI库重新构建 UI 简化 UI 代码，删除冗余代码

--- Fastrans/main.py
+++ Fastrans/main.py
@@ -1,59 +1,36 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5 import QtCore
-from qt_material import apply_stylesheet
-from qtawesome import font
+from qfluentwidgets import Theme, setTheme
 import eng_to_ipa as ipa
 import pyperclip
 import sys
 import ctypes
 
-from ui import Ui_Form
+from ui import Ui
 from translate import Translator
-
-# Extra theme setting
-extra = {
-    'font_family': '微软雅黑',
-    'font_size': '12'
-}
 
 
 # Main window class
-class Win(QMainWindow, Ui_Form):
+class Win(QMainWindow, Ui):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
-        self.pushButton_trans.clicked.connect(self.translate)
-        self.pushButton_trans.setShortcut('Return')  # Shortcut key
+        self.btn_trans.clicked.connect(self.translate)
+        self.btn_trans.setShortcut('Return')  # Shortcut key
         self.now_translation = ""
-
-        # Copy button
-        self.pushButton_copy.setFont(font('fa', 16))
-        self.pushButton_copy.setText("")
-        self.pushButton_copy.clicked.connect(lambda: pyperclip.copy(self.now_translation))
-
-        # Switch theme
-        self.themes = ['dark_cyan.xml', 'light_blue.xml']
-        self.theme_name = ['深色', '浅色']
-        self.theme_index = 1
-        self.pushButton_theme.clicked.connect(self.switch_theme)
-
-        # Topmost
-        self.is_topmost = False
-        self.pushButton_topmost.clicked.connect(self.topmost)
+        self.btn_copy.clicked.connect(lambda: pyperclip.copy(self.now_translation))
+        self.btn_theme.toggled.connect(self.switch_theme)
+        self.btn_topmost.toggled.connect(self.topmost)
 
     # Translate function
     def translate(self):
-        content = self.lineEdit_input.text()
+        content = self.txt_input.text()
 
         # Check if source is empty
-        if content == "":
+        if not content:
             return
 
         # Detect language
-        if 'a' <= content[0] <= 'z' or 'A' <= content[0] <= 'Z':
-            from_lang = 'en'
-            to_lang = 'zh'
-        elif content[0] < '\u4e00' or content[0] > '\u9fff':
+        if content[0] < '\u4e00' or content[0] > '\u9fff':
             from_lang = 'auto'
             to_lang = 'zh'
         else:
@@ -61,44 +38,29 @@ class Win(QMainWindow, Ui_Form):
             to_lang = 'en'
 
         # Translate
-        source = self.comboBox.currentText()
+        source = self.combo_platform.currentText()
         translator = Translator(content=content, from_lang=from_lang, to_lang=to_lang, source=source)
         translation = translator.translate()
-        self.textEdit_result.setText(translation)
+        self.txt_result.setText(translation)
         self.now_translation = translation
 
         # 音标
-        if from_lang == 'zh':
-            result = ipa.convert(translation)
-            self.textEdit_result.append('\n['+result+']')
-        elif from_lang == 'en':
-            result = ipa.convert(content)
-            self.textEdit_result.append('\n['+result+']')
+        result = ipa.convert(translation if from_lang == 'zh' else content)
+        self.txt_result.append('\n[' + result + ']')
 
     # Switch theme function
-    def switch_theme(self):
-        self.theme_index = 1 - self.theme_index
-        apply_stylesheet(self, theme=self.themes[self.theme_index], extra=extra, invert_secondary=True)
-        self.pushButton_theme.setText(self.theme_name[self.theme_index])
-        if self.theme_index == 0:
-            self.comboBox.setStyleSheet("QComboBox{color: white;}")
-        else:
-            self.comboBox.setStyleSheet("QComboBox{color: black;}")
+    def switch_theme(self, is_toggled):
+        self.setStyleSheet("background-color: #272727" if is_toggled else "background-color: #f7f9fc")
+        setTheme(Theme.DARK if is_toggled else Theme.LIGHT)
 
     # Topmost
-    def topmost(self):
-        if self.is_topmost:
-            self.setWindowFlags(QtCore.Qt.Widget)
-            self.show()
-        else:
-            self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
-            self.show()
-        self.is_topmost = not self.is_topmost
+    def topmost(self, is_toggled):
+        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint if is_toggled else QtCore.Qt.Widget)
+        self.show()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    apply_stylesheet(app, theme='light_blue.xml', extra=extra, invert_secondary=True)
     ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("myappid")
     win = Win()
     win.show()
--- README.md
+++ README.md
@@ -4,13 +4,12 @@
 <br>
 <img src="Fastrans/src/icon.png" alt="Fastrans" width="100" height="100">
 <br><br>
-Fastrans 是一个美观简约、方便快速的翻译软件，支持多种语言。
+Fastrans 是一个美观简约、方便快速的翻译软件，支持多平台。
 <br>
-Fastrans is a pretty and fast translation software, which supports most languages.
+Fastrans is a pretty and fast translation software, which supports different platforms.
 <br><br>
 <img alt="GitHub License" src="https://img.shields.io/github/license/YaoqxCN/Fastrans">
 <img alt="GitHub Release" src="https://img.shields.io/github/v/release/YaoqxCN/Fastrans">
-<img alt="GitHub Release Date" src="https://img.shields.io/github/release-date/YaoqxCN/Fastrans">
 <img alt="GitHub top language" src="https://img.shields.io/github/languages/top/YaoqxCN/Fastrans">
 <img alt="GitHub commit activity" src="https://img.shields.io/github/commit-activity/y/YaoqxCN/Fastrans">
 </p>
@@ -18,29 +17,30 @@ Fastrans is a pretty and fast translation software, which supports most language
 
 ## 🔍 预览 / Preview
 
-![preview](view.png)
+![preview](view1.png)
 
 ## ✨ 功能 / Features
 
-- 英汉互译 / English-Chinese translation
-- 其他语种自动翻译中文 / Automatically translate other languages to Chinese
-- 明暗主题切换 / Switch between light and dark mode
-- 可选不同翻译平台（支持必应、有道、搜狗）/ Optional different translation platforms (support Bing, Youdao, Sogou)
-- 可显示音标 / Available to show phonetic symbol
-- 可置顶窗口 / Available to pin the window
+- [x] 英汉互译 / English-Chinese translation
+- [x] 其他语种自动翻译中文 / Automatically translate other languages to Chinese
+- [x] 明暗主题切换 / Switch between light and dark mode
+- [x] 可选不同翻译平台（支持必应、有道、搜狗）/ Optional different translation platforms (support Bing, Youdao, Sogou)
+- [x] 可显示音标 / Available to show phonetic symbol
+- [x] 可置顶窗口 / Available to pin the window
 
 ## ✒ 技术 / Technology
 
 - Python 3.12
 - 使用 translators 库获取不同平台翻译结果 / Use translators to get results from different platforms
 - 使用 PyQt5 构建 GUI / Use PyQt5 to build GUI
-- 使用 qt-material 主题美化界面 / Use qt-material to beautify the GUI
+- 使用 pyqt-fluent-widgets 主题美化界面 / Use pyqt-fluent-widgets to beautify the GUI
 - 使用 eng_to_ipa 显示音标 / Use eng_to_ipa to show phonetic symbol
 
 ## 🎯 待办 / Todo
 
 **欢迎 PR！**
 
-- 翻译历史记录 / Translation history
-- 最小化托盘 / Minimize to tray
-- 快捷键弹出窗口 / Pop up window with shortcut keys
+- [ ] 翻译历史记录 / Translation history
+- [ ] 最小化托盘 / Minimize to tray
+- [ ] 快捷键弹出窗口 / Pop up window with shortcut keys
+- [ ] 优化 UI 布局 / Optimize UI layout
